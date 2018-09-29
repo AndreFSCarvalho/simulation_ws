597c606364e804aa778d6f337c5afd09ee0eb634
André <andrelipecarvalho@gmail.com>
2018-09-29 14:49:14 -0300

    aula3

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/andre/simulation_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/andre/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/andre/catkin_ws/devel;/opt/ros/kinetic" != "" else []
